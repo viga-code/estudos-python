4d12d2c1a5d89fefe74058ddad0b08e9ff08afcf
viga.code <84135441+viga-code@users.noreply.github.com>
2026-07-22 13:48:06 -0300

    Conceitos iniciais python

---
+++ conceitos_iniciais.py
@@ -0,0 +1,23 @@
+# . print(type ('vitinho'))#type = tipo
+#20 = int = inteiro
+#20.01 = float = 
+#"qualquercoisa" = str = string = texto
+
+##.primeiro_numero = 2
+#.segundo_numero = 5
+
+#.print(primeiro_numero+segundo_numero) 
+#.print(2-5)
+#.print(2*5)
+##.print(2/5)
+
+# {+=+} {-=-} {*=x} {/=divisão} {**=ºpotencia} {%=traz o resto da divisão}
+
+#.nome = input("Qual é o seu nome ?") #input = faz como uma pergunta(pede um valor)
+#.print("Salve "+nome)
+
+num1 = int(input("Digite um numero:"))    #sem o int, ele puxa tudo como str(texto)
+num2 = int(input("Digite outro numero:"))
+resultado_soma = num1 + num2
+print(resultado_soma)# ou print(f"O resuldado da soma {num1} e {numm2} é {resultado_soma}")
+
